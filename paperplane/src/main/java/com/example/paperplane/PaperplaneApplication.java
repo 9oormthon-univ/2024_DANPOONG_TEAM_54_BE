eb2580aa65312c118298e028decb84a041ce1eaa
Kyuwon-Choi <kyuwon30@naver.com>
2024-11-18 00:52:50 +0900

    initial commit

---
+++ paperplane/src/main/java/com/example/paperplane/PaperplaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.paperplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperplaneApplication.class, args);
+	}
+
+}
---
+++ paperplane/src/test/java/com/example/paperplane/PaperplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paperplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
