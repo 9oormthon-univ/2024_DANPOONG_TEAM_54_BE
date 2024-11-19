7d345246b5d1fbf577d85ad94f4ceab58bfae03b
Kyuwon-Choi <kyuwon30@naver.com>
2024-11-19 19:13:03 +0900

    fix: 카카오 로그인 시 정보 안받아오는 문제 해결

--- paperplane/src/main/java/com/example/paperplane/domain/auth/dto/KakaoUserInfoResponse.java
+++ paperplane/src/main/java/com/example/paperplane/domain/auth/dto/KakaoUserInfoResponse.java
@@ -1,18 +1,31 @@
 package com.example.paperplane.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record KakaoUserInfoResponse(
         String id,
-        KakaoAccount kakaoAccount
+        @JsonProperty("connected_at") String connectedAt,
+        Properties properties,
+        @JsonProperty("kakao_account") KakaoAccount kakaoAccount
 ) {
+    public record Properties(
+            @JsonProperty("profile_image") String profileImage,
+            @JsonProperty("thumbnail_image") String thumbnailImage
+    ) {
+    }
+
     public record KakaoAccount(
-            Profile profile,
-            String email
+            @JsonProperty("profile_image_needs_agreement") boolean profileImageNeedsAgreement,
+            Profile profile
     ) {
     }
 
     public record Profile(
-            String nickname,
-            String profileImageUrl
+            @JsonProperty("thumbnail_image_url") String thumbnailImageUrl,
+            @JsonProperty("profile_image_url") String profileImageUrl,
+            @JsonProperty("is_default_image") boolean isDefaultImage
     ) {
     }
 }
