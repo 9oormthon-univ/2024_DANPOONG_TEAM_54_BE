a4c36b8a9aba54d3168671d09f5932870ce7747c
Kyuwon-Choi <kyuwon30@naver.com>
2024-11-22 19:41:28 +0900

    chore 배포환경에 맞춰 설정

--- paperplane/src/main/java/com/example/paperplane/domain/file/service/S3Service.java
+++ paperplane/src/main/java/com/example/paperplane/domain/file/service/S3Service.java
@@ -24,7 +24,7 @@ public class S3Service {
 
     @Value("${S3_BUCKET_NAME:paperplane-s3-bucket}")
     private String bucketName;
-    //test
+    //testdd
 
 
     public String uploadFile(MultipartFile file) {
